071a5efcf67e897dc1260d641b202ad3b56ef3d7
Taisiia <taisionok@gmail.com>
2024-10-10 21:04:06 +0200

    commit

--- src/main/java/de/telran/_lesson_JavaForQA_4_20241004/Povtorenie/CompanyApp.java
+++ src/main/java/de/telran/_lesson_JavaForQA_4_20241004/Povtorenie/CompanyApp.java
@@ -1,5 +1,7 @@
 package de.telran._lesson_JavaForQA_4_20241004.Povtorenie;
 
+import module1.lecture1.module1.lecture2.lecture2_3.Employee;
+
 import java.util.ArrayList;
 
 public class CompanyApp {
@@ -30,8 +32,8 @@ public class CompanyApp {
 //        c1.add(new Employee("Julia", "Dev", 2002));
 //        c1.add(new Employee("Liz", "HR", 2007));
 
-    public static void printEmployees (ArrayList<Employee> listEmployee){
-        for (Employee e: listEmployee) {
+    public static void printEmployees(ArrayList<Employee> listEmployee) {
+        for (Employee e : listEmployee) {
             System.out.println(e.name + "(" + e.position + ") : " + e.year);
         }
     }
@@ -40,13 +42,24 @@ public class CompanyApp {
         String name;
         String position;
         int year;
+        double salary;
 
-        public Employee(String name, String position, int year) {
+        public Employee(String name, String position, int year, double salary) {
             this.name = name;
             this.position = position;
             this.year = year;
+            this.salary = salary;//
+        }
+       // Добавить к классу Employee поле salary с геттером и сеттером,
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
         }
     }
+
 
     public static class Company {
         String name;
@@ -84,31 +97,55 @@ public class CompanyApp {
 
         public ArrayList<Employee> findByPosition(String position) {
             ArrayList<Employee> result = new ArrayList<>();
-
-
-            //
-
+            for (Employee e : employees) {
+                if (e.position.equals(position)) {
+                    result.add(e);
+                }
+            }
             return result;
         }
 
         public ArrayList<Employee> findHiredAfter(int year) {
             ArrayList<Employee> result = new ArrayList<>();
-
-
-            //
-
+            for (Employee e : employees) {
+                if (e.year > year) {
+                    result.add(e);
+                }
+            }
+            return result;
+        }
+        // , а в класс Company - метод salaryTotal.
+        // Метод salaryTotal должен возвращать суммарнуую зарплату всех сотрудников.
+        public double salaryTotal() {
+            double total =0;
+            for (Employee e: employees){
+                total +=e.getSalary();
+            }
+            return total;
+        }
+        //Написать для класса Company метод salaryAbove, в который передаётся Employee
+        // и он возвращает список сотрудников (ArrayList), у которых зарплаты выше,
+        // чем у указанного сотрудника.
+        public ArrayList<Employee> salaryAbove(Employee employee) {
+            ArrayList<Employee> result = new ArrayList<>();
+            for (Employee e : employees) {
+                if (e.getSalary() > employee.getSalary()) {
+                    result.add(e);
+                }
+            }
             return result;
         }
     }
+
 
     public static void main(String[] args) {
         Company c1 = new Company("Happy Train");
         Company c2 = new Company("Bubbles");
 
-        c1.add(new Employee("John", "Dev", 2000));
-        c1.add(new Employee("Bob", "QA", 2015));
-        c1.add(new Employee("Julia", "Dev", 2002));
-        c1.add(new Employee("Liz", "HR", 2007));
+        c1.add(new Employee("John", "Dev", 2000, 3000.30));
+        c1.add(new Employee("Bob", "QA", 2015, 2000.20));
+        c1.add(new Employee("Julia", "Dev", 2002, 4000.40));
+        c1.add(new Employee("Liz", "HR", 2007, 1800.18));
 
         c1.printEmployees("выводит список сотрудников на экран");
 
@@ -117,7 +154,7 @@ public class CompanyApp {
         c1.printEmployees("выводит список сотрудников на экран без Julia");
 
         // берем на работу Rita
-        c1.add(new Employee("Rita", "CTO", 2024));
+        c1.add(new Employee("Rita", "CTO", 2024, 2500.50));
         c1.printEmployees("выводит список сотрудников на экран  с Rita");
 
 //        Company[] companies = new Company[]{};
@@ -126,8 +163,18 @@ public class CompanyApp {
         printEmployees(devs);
         ArrayList<Employee> after2005 = c1.findHiredAfter(2005);
         printEmployees(after2005);
+
+        // Выводим общую зарплату всех сотрудников компании
+        System.out.println("Суммарная зарплата сотрудников: " + c1.salaryTotal());
+
+
+        Employee targetEmployee = new Employee("Rita", "CTO", 2024, 2500.50);
+        ArrayList<Employee> employeesAbove = c1.salaryAbove(targetEmployee);
+        System.out.println("Сотрудники с зарплатой выше, чем у " + targetEmployee.name + ":");
+        printEmployees(employeesAbove);
     }
 }
+
 
 
 
